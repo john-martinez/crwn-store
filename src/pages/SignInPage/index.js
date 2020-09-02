9e4c20b06fd23f90dcf9d33e35046e36e57154de
johnray-martinez <jrvmartinez@icloud.com>
2020-09-01 22:05:23 -0400

    finished styling sign in page

--- src/pages/SignInPage/index.js
+++ src/pages/SignInPage/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SignInForm from '../../components/SignInForm';
 import SignUpForm from '../../components/SignUpForm';
+import './index.scss';
 
 export default function SignInPage() {
   return(
-    <main>
+    <main className="sign-in-page">
       <SignInForm />
       <SignUpForm />
     </main>
