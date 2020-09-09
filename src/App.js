f3925f95cd79aac610c74ca73ab75c6f1165c485
John Ray Martinez <jrvmartinez@icloud.com>
2020-09-08 22:20:41 -0400

    created function that adds the user to the database

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import DetailsPage from './pages/DetailsPage';
 import ShopPage from './pages/ShopsPage';
 import SignInPage from './pages/SignInPage';
 import Header from './components/Header';
-import { auth, firestore } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   state = { currentUser: null }
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    auth.onAuthStateChanged(async user => {
+      const userRef = await createUserProfileDocument(user)
+      userRef.onSnapshot(snapShot => {
+        this.setState({ currentUser: {
+          id: snapShot.id,
+          ...snapShot.data()
+        }})
+      })
     })
 
-    const test = firestore.doc('/test/JCalSifQJzxNltADzXJX').get();
     
-    test.then(data=>console.log(data))
   }
   render() {
     const { currentUser } = this.state;
